afb9e6315819aa339ebe8e76195be863368a5f7e
Amauri Blanco Correa <75027271+AmauriBlanco@users.noreply.github.com>
2021-09-30 18:31:19 -0300

    Add files via upload

---
+++ album/script.js
@@ -0,0 +1,14 @@
+var action = document.getElementById("hiddenButton");
+
+function hiddenMenu() { 
+    if (action.style.height === "230px") {
+        action.style.height = "0";
+    } else {
+      action.style.height = "230px";
+    }
+    
+} 
+
+  
+
+  
